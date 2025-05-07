4e538f623f1efdb833b27dbd0a07f2c2eafe0ea8
Fia <94110257+fiababiakandersson@users.noreply.github.com>
2025-05-07 11:15:39 +0200

    prep components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Header from './components/Header.vue'
 import Home from './components/Home.vue'
+// import Info from './components/Info.vue'
+// import Table from './components/Table.vue'
 </script>
 
 <template>
   <Header />
+  <!-- <Info />
+  <Table /> -->
   <Home />
 </template>
-
-
